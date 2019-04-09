1a02b5d0f0b3838f6f8c06b69349e725dbff3bba
Rahul Lal <Rahul21Lal@gmail.com>
2019-04-10 09:37:34 +1200

    Functionality working
    Issue was declaring the script.js file at end of html file before 'body' closing tag.
    
    Presumably have to do with collecting the ID names of all html items present to place into the respected js variables.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web Calculator
+
+
+09/04/2019: Initated web calculator project & respository on Github
+10/04/2019: Successfully get calculator to work all functions via tutorial video, now my imagination must lead the rest of the way
---
+++ script.js
@@ -0,0 +1,61 @@
+var num1, num2, sum, result;
+
+//Functionality for addition button
+plusButton = document.getElementById("btnAddition");
+minusButton = document.getElementById("btnSubtaction");
+multiplyButton = document.getElementById("btnMultiplication");
+divideButton = document.getElementById("btnDivision");
+
+
+plusFunction = function()
+{
+
+    num1 = document.getElementById("txtNum01");
+    num2 = document.getElementById("txtNum02");
+    
+    sum = parseInt(num1.value) + parseInt(num2.value);
+    
+    result = document.getElementById("h2Result");
+    result.innerText = sum;
+}
+
+minusFunction = function()
+{
+
+    num1 = document.getElementById("txtNum01");
+    num2 = document.getElementById("txtNum02");
+    
+    sum = parseInt(num1.value) - parseInt(num2.value);
+    
+    result = document.getElementById("h2Result");
+    result.innerText = sum;
+}
+
+multiplyFunction = function()
+{
+
+    num1 = document.getElementById("txtNum01");
+    num2 = document.getElementById("txtNum02");
+    
+    sum = parseInt(num1.value) * parseInt(num2.value);
+    
+    result = document.getElementById("h2Result");
+    result.innerText = sum;
+}
+
+divideFunction = function()
+{
+
+    num1 = document.getElementById("txtNum01");
+    num2 = document.getElementById("txtNum02");
+    
+    sum = parseInt(num1.value) / parseInt(num2.value);
+    
+    result = document.getElementById("h2Result");
+    result.innerText = sum;
+}
+
+plusButton.addEventListener("click", plusFunction);
+minusButton.addEventListener("click", minusFunction);
+multiplyButton.addEventListener("click", multiplyFunction);
+divideButton.addEventListener("click", divideFunction);
