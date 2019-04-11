25148ae0eefe09e1072341e3860f84608869cf50
Rahul Lal <Rahul21Lal@gmail.com>
2019-04-11 14:49:59 +1200

    Changing Link
    Change reposititory name & link address

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var num1, num2, sum, result;
+var num1, num2, sum, result, resultObject;
 
 // Functionality for addition button
 plusButton = document.getElementById("btnAddition");
@@ -7,52 +7,43 @@ multiplyButton = document.getElementById("btnMultiplication");
 divideButton = document.getElementById("btnDivision");
 
 
-plusFunction = function()
-{
-    // Collecting values from both text boxes
-    num1 = document.getElementById("txtNum01");
-    num2 = document.getElementById("txtNum02");
-
-    sum = parseInt(num1.value) + parseInt(num2.value);
-    
-    result = document.getElementById("h2Result");
-    result.innerText = sum;
-}
-
 minusFunction = function()
 {
-    // Collecting values from both text boxes
-    num1 = document.getElementById("txtNum01");
-    num2 = document.getElementById("txtNum02");
-
     sum = num1.value - num2.value;
-    
-    result = document.getElementById("h2Result");
-    result.innerText = sum;
 }
 
 multiplyFunction = function()
 {
-    // Collecting values from both text boxes
-    num1 = document.getElementById("txtNum01");
-    num2 = document.getElementById("txtNum02");
-
     sum = num1.value * num2.value;
-    
-    result = document.getElementById("h2Result");
-    result.innerText = sum;
 }
 
 divideFunction = function()
 {
-    // Collecting values from both text boxes
-    num1 = document.getElementById("txtNum01");
-    num2 = document.getElementById("txtNum02");
-
     sum = num1.value / num2.value;
-    
+}
+
+plusFunction = function()
+{
+
+    sum = parseInt(num1.value) + parseInt(num2.value);
+}
+
+showResults = function(result)
+{
     result = document.getElementById("h2Result");
     result.innerText = sum;
+}
+
+gatherNumerals = function()
+{
+    // Collecting values from both text boxes
+    num1 = document.getElementById("txtNum01").value;
+    num2 = document.getElementById("txtNum02").value;
+    resultObject =  {
+        "Number 1": num1,
+        "Number 2": num2
+    }
+    return resultObject;
 }
 
 plusButton.addEventListener("click", plusFunction);
